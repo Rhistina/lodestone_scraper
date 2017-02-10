87d9b4409ef0084eed48b581a277a62e07617563
Orenshi <rhistina@gmail.com>
2017-02-09 22:46:57 -0500

    Commit setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lodestone_scraper',
+      version='0.1',
+      description='A simple Python API to scrape the FFXIV Lodestone website',
+      author='Rhistina Revilla',
+      author_email='rhistina@gmail.com',
+      url='https://github.com/Rhistina/lodestone_scraper/',
+      py_modules = ['lodestone_scraper']
+     )
